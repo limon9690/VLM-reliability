bac00de13960d3b6f59460c1a90a17ac6bdebae4
limon9690 <limon.hossain26@yahoo.com>
2026-08-20 03:38:06 +0800

    Causal (masked) self-attention via causal flag

--- src/attention.py
+++ src/attention.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 
 class SelfAttention(nn.Module):
 
-    def __init__(self, dim_in, dim_out):
+    def __init__(self, dim_in, dim_out, causal = False):
         super().__init__()
+        self.causal = causal
         self.w_q = nn.Linear(dim_in, dim_out, bias=False)
         self.w_k = nn.Linear(dim_in, dim_out, bias=False)
         self.w_v = nn.Linear(dim_in, dim_out, bias=False)
@@ -16,6 +17,12 @@ class SelfAttention(nn.Module):
         V = self.w_v(X)
 
         scores = Q @ K.transpose(-2, -1) / (K.shape[-1] ** 0.5)
+
+        if self.causal:
+            seq_len = scores.shape[-1]
+            mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
+            scores = scores.masked_fill(mask, float('-inf'))
+
         weights = torch.softmax(scores, dim=-1)
         context = weights @ V
         
